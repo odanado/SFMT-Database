8acc7603c8c48c1e2bd9611e35d93e63da1e5b16
odan <odan3240@gmail.com>
2018-03-04 23:16:16 +0900

    fix

--- utils.h
+++ utils.h
@@ -14,6 +14,6 @@ string getFilename(const string &, int);
 template <typename T>
 string toHex(T num, uint32_t radix) {
     std::stringstream ss;
-    ss << std::hex << std::setw(radix) << std::setfill('0') << num;
+    ss << std::uppercase << std::hex << std::setw(radix) << std::setfill('0') << num;
     return ss.str();
 }
